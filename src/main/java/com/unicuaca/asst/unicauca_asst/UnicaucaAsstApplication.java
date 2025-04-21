108d28b8a582c5f86a9ee380872ef8086590ba6d
xander98in <leyderin@unicauca.edu.co>
2025-04-21 16:08:10 -0500

    Asst - Creación del proyecto

---
+++ src/main/java/com/unicuaca/asst/unicauca_asst/UnicaucaAsstApplication.java
@@ -0,0 +1,13 @@
+package com.unicuaca.asst.unicauca_asst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicaucaAsstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnicaucaAsstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unicuaca/asst/unicauca_asst/UnicaucaAsstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicuaca.asst.unicauca_asst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnicaucaAsstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
