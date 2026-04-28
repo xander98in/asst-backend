f55245d0a651fbf85bf59f809d5e770109ba0acf
xander98in <leyderin@unicauca.edu.co>
2026-04-28 10:44:57 -0500

    Deploy - Ajuste en url de swagger para deploy

--- src/main/java/com/unicuaca/asst/unicauca_asst/common/config/OpenApiConfig.java
+++ src/main/java/com/unicuaca/asst/unicauca_asst/common/config/OpenApiConfig.java
@@ -30,7 +30,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
     ),
     servers = {
         @Server(url = "http://localhost:8080", description = "Local"),
-        @Server(url = "https://https://asst-backend-kwvb.onrender.com", description = "Prod")
+        @Server(url = "https://asst-backend-kwvb.onrender.com", description = "Prod")
     }
 )
 @SecurityScheme(
